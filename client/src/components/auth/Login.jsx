77369a7218e56ba22e31c7909c5e3267df270ad5
Ronak Talati <ronakjain980@gmail.com>
2026-03-20 01:35:58 +0530

    demo

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        username: 'demo_admin',
+        username: 'demo',
         password: 'demo123'
       })
     });
