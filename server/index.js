74e03ccf4c512d33568583b3bce3bb3113933207
Ronak Talati <ronakjain980@gmail.com>
2026-03-20 01:01:46 +0530

    git

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors({ origin: ['http://localhost:3000','http://localhost:3001'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000','https://medical-crm-codecrafters.netlify.app'], credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth',         require('./routes/auth'));
