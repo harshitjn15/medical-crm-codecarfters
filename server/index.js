d58e1b9857cc59e07dbdb606f02943c32eae298c
Ronak Talati <ronakjain980@gmail.com>
2026-03-20 18:19:02 +0530

    prod

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const connectDB = require('./database/connect');
 
 const app  = express();
 const PORT = process.env.PORT || 5000;
+const isProd = process.env.NODE_ENV === 'production';
 
 connectDB();
 
